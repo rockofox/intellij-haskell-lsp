eb9900dbe6e596f51eb2dfff7a8d16625afe8698
rockofox <ofelix@pm.me>
2024-07-14 01:23:50 +0200

    More stable HLS location logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "boo.fox"
-version = "1.3.1"
+version = "1.3.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/boo/fox/haskelllsp/HaskellLanguageServerFactory.kt
+++ src/main/kotlin/boo/fox/haskelllsp/HaskellLanguageServerFactory.kt
@@ -3,43 +3,36 @@ package boo.fox.haskelllsp
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
+import com.intellij.util.EnvironmentUtil
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
 import com.redhat.devtools.lsp4ij.LanguageServerManager
 import com.redhat.devtools.lsp4ij.server.ProcessStreamConnectionProvider
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
-import okio.IOException
+import java.io.File
+import java.nio.file.Paths
+import kotlin.io.path.pathString
 
-class HaskellLanguageServerFactory: LanguageServerFactory {
-    override fun createConnectionProvider(project: Project): StreamConnectionProvider =
-        HaskellLanguageServer(project)
+class HaskellLanguageServerFactory : LanguageServerFactory {
+    override fun createConnectionProvider(project: Project): StreamConnectionProvider = HaskellLanguageServer(project)
 }
 
-class HaskellLanguageServer(private val project: Project): ProcessStreamConnectionProvider() {
-
-    private fun findHls(): String = if (try { ProcessBuilder(LANGUAGE_SERVER_COMMAND, "--version").start() } catch (_: IOException) { null }?.waitFor() == 0) {
-        LANGUAGE_SERVER_COMMAND
-    } else {
-        (try { ProcessBuilder(GHCUP_COMMAND, "whereis", "hls").start() } catch (_: IOException) { null })?.takeIf { it.waitFor() == 0 }?.inputReader()?.readText()
-            ?.trim().let {
-                if(it == null) {
-                    NotificationGroupManager.getInstance()
-                        .getNotificationGroup("Haskell LSP")
-                        .createNotification("Haskell LSP", "Haskell Language Server not found. Make sure it is installed properly (and is available in PATH or via ghcup), and restart the IDE.", NotificationType.ERROR)
-                        .notify(project)
-                    LanguageServerManager.getInstance(project).stop("haskellLanguageServer")
-                    throw IllegalStateException("Haskell Language Server not found")
-                } else {
-                    it
-                }
-            }
-    }
+class HaskellLanguageServer(project: Project) : ProcessStreamConnectionProvider() {
+    private fun findExecutableInPATH(executable: String) =
+        EnvironmentUtil.getEnvironmentMap().values.flatMap { it.split(File.pathSeparator) }
+            .map { File(Paths.get(it, executable).pathString) }.find { it.exists() && it.canExecute() }?.path
 
     init {
-        super.setCommands(listOf(findHls(), "--lsp"))
-    }
-
-    companion object {
-        const val LANGUAGE_SERVER_COMMAND = "haskell-language-server-wrapper"
-        const val GHCUP_COMMAND = "ghcup"
+        val hlsPath = findExecutableInPATH("haskell-language-server-wrapper")
+        if (!hlsPath.isNullOrEmpty()) {
+            super.setCommands(listOf(hlsPath, "--lsp"))
+            super.setWorkingDirectory(project.basePath)
+        } else {
+            NotificationGroupManager.getInstance().getNotificationGroup("Haskell LSP").createNotification(
+                    "Haskell LSP",
+                    "Haskell Language Server not found. Make sure it is installed properly (and is available in PATH), and restart the IDE.",
+                    NotificationType.ERROR
+                ).notify(project)
+            LanguageServerManager.getInstance(project).stop("haskellLanguageServer")
+        }
     }
 }
